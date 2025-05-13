6585302682d7efe038f040ce44c03429fbda3a23
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-13 14:13:57 +0200

    Add security.py file and add implementation for password hashing and verification

---
+++ app/core/security.py
@@ -0,0 +1,13 @@
+from bcrypt import hashpw, gensalt, checkpw
+
+class Hasher():
+    @staticmethod
+    def hash_password(password: str) -> str:
+        return hashpw(password.encode("utf-8"), gensalt()).decode("utf-8")
+
+    @staticmethod
+    def verify_password(plain_password: str, hashed_password: str) -> bool:
+        try:
+            return checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+        except ValueError:
+            return False
