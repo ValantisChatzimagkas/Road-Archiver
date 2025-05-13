592511a76f90a1771da921bc95aea17e92f4ef65
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-13 14:32:11 +0200

    Add field for user roles

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,21 @@
-from sqlalchemy import Column, Integer, String
+from enum import Enum
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import Enum as SqlEnum
+
 from app.core.database import Base
+
+
+class UserRolesOptions(str, Enum):
+    """
+    ADMIN: 	Full control: manage users, system settings, content, etc
+    MODERATOR: Limited admin: review/manage content or users (but not system settings)
+    USER: Regular authenticated user
+    GUEST: Unauthenticated or very limited account
+    """
+    ADMIN = "ADMIN"
+    MODERATOR = "MODERATOR"
+    USER = "USER"
+    GUEST = "GUEST"
 
 
 class User(Base):
@@ -9,3 +25,4 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    role = Column(SqlEnum(UserRolesOptions), default=UserRolesOptions.USER, nullable=False)
