164dfb4088545e9c0806e57bf39edca89ac93a33
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-14 12:17:29 +0200

    Add current user access to get _user endpoint

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.api.v1.services import users_service
-from app.api.v1.services.authentication_service import get_current_user
 from app.core.database import get_db
-from app.db.models import User
 from app.schemas import CreateUser, ReadUser
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -16,8 +14,8 @@ async def create_user(request: CreateUser, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=ReadUser)
-async def get_user(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    user = await users_service.get_user_by_id(user_id=id, db=db, current_user=current_user)
+async def get_user(id: int, db: Session = Depends(get_db)):
+    user = await users_service.get_user_by_id(user_id=id, db=db)
     return user
 
 
