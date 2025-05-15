f62356a6e2d408a9b21c36547f5cdf3dbdb5d6da
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-15 02:53:06 +0200

    Add extra documentation to endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,27 +1,58 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body, Response, status
 from sqlalchemy.orm import Session
 from app.api.v1.services import users_service
 from app.api.v1.services.authentication_service import get_current_user
 from app.core.database import get_db
-from app.db.models import User
+from app.db.models import User, UserRolesOptions
 from app.schemas import CreateUser, ReadUser
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post("/", response_model=ReadUser)
-async def create_user(request: CreateUser, db: Session = Depends(get_db)):
+@router.post("/", response_model=ReadUser, summary="Create a new user",
+             description="Creates a user with the provided details.",
+             responses={
+                 status.HTTP_201_CREATED: {"description": "User successfully created"}
+             }
+             )
+async def create_user(request: CreateUser = Body(...,
+                                                 example=dict(
+                                                     username="my_user",
+                                                     email="my_user@coldmail.com",
+                                                     hashed_password="A_sword",
+                                                     role=UserRolesOptions.USER)
+                                                 ),
+                      db: Session = Depends(get_db)):
     created_user = await users_service.create_user(request=request, db=db)
     return created_user
 
 
-@router.get("/{id}", response_model=ReadUser)
+@router.get("/{id}", response_model=ReadUser, summary="Get user by id",
+            description="""
+             This endpoint has 2 modes. 
+             <br>
+             <ul>
+                <li> if user is **Admin** then that user can get every user. </li>
+                <li> if user is **User** then this user can retrieve only itself.</li>
+             </ul>
+             """,
+            responses={
+                status.HTTP_200_OK: {"description": "Found User"},
+                status.HTTP_401_UNAUTHORIZED: {"description": "Not Allowed"}
+            })
 async def get_user(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     user = await users_service.get_user_by_id(user_id=id, db=db, current_user=current_user)
     return user
 
 
-@router.delete("/{id}")
-async def delete_user(id:int, db: Session = Depends(get_db)):
+@router.delete("/{id}",
+               summary="Deletes a user",
+               description="Delete a user, in order to user this endpoint, the caller must be an Admin",
+               responses={
+                   status.HTTP_200_OK: {"description": "User was deleted"},
+                   status.HTTP_401_UNAUTHORIZED: {"description": "Not Allowed"}
+               }
+               )
+async def delete_user(id: int, db: Session = Depends(get_db)):
     result = await users_service.delete_user(db=db, user_id=id)
     return result
