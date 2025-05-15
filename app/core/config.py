7d96726dceb32551fc0113e0d041b6e602cf1329
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-16 00:25:43 +0200

    Add default values to config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,11 @@
 from dynaconf import Dynaconf
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "roadnetworks")
 
 settings = Dynaconf(
     settings_files=["settings.toml", ".env"],
@@ -6,8 +13,14 @@ settings = Dynaconf(
     load_dotenv=True,
 )
 
-# Compose the DB URL manually
+settings.DB_HOST = DB_HOST
+settings.DB_PORT = DB_PORT
+settings.DB_USER = DB_USER
+settings.DB_PASSWORD = DB_PASSWORD
+settings.DB_NAME = DB_NAME
+
 settings.DB_URL = (
     f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
+
