ec04ff617a0c82d75c33cce30faa49499c19c861
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-15 03:11:23 +0200

    Add openapi_tags to API

--- app/main.py
+++ app/main.py
@@ -6,18 +6,25 @@ from app.core.database import engine
 from app.db import models
 from app.api.v1.endpoints.users import router as users_router
 from app.api.v1.endpoints.authentication import router as authentication_router
+from app.api.v1.endpoints.road_networks import router as road_networks_router
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
-
 app = FastAPI(title="Road Network Management API",
               description="REST API for managing road networks, uploading, updating and retrieving road networks",
-              version="1.0.0", )
+              version="1.0.0",
+              openapi_tags=[
+                  {"name": "users", "description": "Operations related to users"},
+                  {"name": "authentication", "description": "Login and security"},
+                  {"name": "Networks", "description": "Manage road network data"},
+              ]
+              )
 
 app.include_router(users_router)
 app.include_router(authentication_router)
+app.include_router(road_networks_router)
 
 models.Base.metadata.create_all(engine)
 
