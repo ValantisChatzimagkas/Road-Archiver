d4f7adc6233b06b6b9dfe0b32f1a3d243d963e8e
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-26 01:53:38 +0200

    Remove this, it shouldn't be there and it is also redundant, we already have in main.py a healthcheck endpoint

--- app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/", tags=["Health"])
-async def root():
-    """Health check endpoint."""
-    return {"status": "healthy", "message": "Road Network API is running"}
