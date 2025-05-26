6f5d3d7e7f542f3dc40269a6b47396de6321fc52
ValantisChatzimagkas <valantis.hatzimagkas@gmail.com>
2025-05-26 12:18:41 +0200

    Add log if rollback happened

--- app/core/database.py
+++ app/core/database.py
@@ -1,31 +1,28 @@
-from typing import Any, Generator
-
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from app.core.config import settings
+import logging
 
 engine = create_engine(settings.DB_URL, echo=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+logger = logging.getLogger(__name__)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db() -> Generator[Session, Any, None]:
-    """
-    Used for database dependency injection.
-    :return:
-    """
+def get_db():
     db = SessionLocal()
     try:
         yield db
-    except SQLAlchemyError:
+    except SQLAlchemyError as e:
         db.rollback()
+        logger.exception("DB rollback due to error")
         raise
     finally:
         db.close()
